f87fe7f1429a8e5dfb4f9ecddf9e18e0da14eca8
Ben <ben@B3nj4m1nU.(none)>
2010-09-30 13:27:00 -0400

    js integrated to index.php rev 2

--- functions.js
+++ functions.js
@@ -1,45 +1,15 @@
-function display_success() {
+function display_success(text) {
 	document.getElementById("overlay").style.display="block";
 	var box = document.getElementById("success_box");
-	document.getElementById("sContent").innerHTML="This is the message you get if you succeeded";
+	document.getElementById("sContent").innerHTML=text;
 	box.style.display = "block";
 }
 
-function display_failure() {
+function display_failure(text) {
 	document.getElementById("overlay").style.display="block";
 	var box = document.getElementById("fail_box");
-	document.getElementById("fContent").innerHTML="This is the message you get if you failed";
+	document.getElementById("fContent").innerHTML=text;
 	box.style.display = "block";
-}
-function display_success1() {
-	document.getElementById("overlay").style.display="block";
-	var box = document.getElementById("success_box");
-	
-	var boxhead = document.createElement("h1");
-	box.appendChild(boxhead);
-	boxhead.innerHTML='Success!';
-	
-	var boxtext = document.createElement("p");
-	box.appendChild(boxtext);
-	boxtext.innerHTML='This is the message you get if you succeeded';
-
-	box.style.display = "block";
-}
-
-function display_failure1() {
-	document.getElementById("overlay").style.display="block";
-	var box = document.getElementById("fail_box");
-	
-	var boxhead = document.createElement("h1");
-	boxhead.innerHTML="Epic Fail!";
-	box.appendChild(boxhead);
-	
-	var boxtext = document.createElement("p");
-	box.appendChild(boxtext);
-	boxtext.innerHTML="This is the message you get if you failed";
-
-	box.style.display = "block";
-	
 }
   
 function close() {
