d27a0985bdad510a333ff53897c0f06066a941aa
Ben Cole <wengole@gmail.com>
2010-10-11 16:28:12 +0100

    * classes/EveMurmurUser.php: created class to handle data

---
+++ classes/EveMurmurUser.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EveMurmurUser
+ *
+ * @author ben
+ */
+class EveMurmurUser {
+    //put your code here
+}
+?>
