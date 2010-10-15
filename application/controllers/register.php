8d4e60324136cad3d5d9284fa4eb1e055a3bd063
Ben Cole <wengole@gmail.com>
2010-10-15 17:58:00 +0100

    * More CSS "fixes" * More work on view * Registration "library" added to be used as an object to hold form data * Register controller work, added function that will handle registration

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -2,22 +2,20 @@
 
 class Register extends Controller {
 
-	function Register()
-	{
-		parent::Controller();
-                $this->load->helper(array('html', 'form'));
-	}
+    function Register() {
+        parent::Controller();
+        $this->load->helper(array('html', 'form'));
+        
+    }
 
-	function index()
-	{
-            $this->load->model('User','',TRUE);
-            $this->load->model('Registration');
-            $form_config = array (
-                'uname_array' => $this->Registration->getUname_array(),
-                'selected_user' => $this->Registration->getSelected_user(),
-                'userid' => $this->Registration->getUserid(),
-                'apikey' => $this->Registration->getApikey()
-            );
-            $this->load->view('registerview', $form_config);
-	}
+    function index() {
+        $this->load->library('Registration');
+        $reg = new Registration();
+        $this->load->view('registerview', $reg);
+    }
+
+    function add() {
+        echo "test";
+    }
+
 }
