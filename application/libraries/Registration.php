9a7c894629f2acd76cedcbd036f20d21ba9d8253
Ben Cole <wengole@gmail.com>
2010-10-15 16:34:34 +0100

    * Moved classes to libraries for CodeIgniter compatability

---
+++ application/libraries/Registration.php
@@ -0,0 +1,13 @@
+<?php
+class Registration {
+
+    public $uname_array;
+    public $selected_user;
+    public $userid;
+    public $apikey;
+    public $username;
+    public $password;
+    public $password2;
+
+}
+?>
