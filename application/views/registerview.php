4a82b1ea2c5bbf7bf188e7b70c52e4eceec0aca6
Ben Cole <wengole@gmail.com>
2010-10-16 14:27:40 +0100

    * More work on form, added getSelectedUser function to Registration class

--- application/views/registerview.php
+++ application/views/registerview.php
@@ -5,14 +5,16 @@
             <br />
             <?= form_open('register/add'); ?>
             <p>User ID:</p>
-            <?= form_input(array('name'=>'userid','class'=>'userinput','value'=>$userid)); ?>
+            <?= form_input(array('name'=>'userid','id'=>'userid','class'=>'userinput','value'=>$userid)); ?>
             <p>Limited API:</p>
-            <?= form_input(array('name'=>'apikey','class'=>'userinput','value'=>$apikey)); ?>
+            <?= form_input(array('name'=>'apikey','id'=>'apikey','class'=>'userinput','value'=>$apikey)); ?>
             <?php if (!empty($uname_array)): ?>
             <p>Pick Character:</p>
             <?= form_dropdown('username', $uname_array, $selected_user, 'class="userselect"'); ?>
             <?php endif; ?>
-
+            <?php if (!empty($username) || $password != $password2): ?>
+            <?= form_password(array('name'=>'password',)); ?>
+            <?php endif; ?>
             <div class='buttons'>
                 <?= form_button(array(
                     'name'=>'save',
