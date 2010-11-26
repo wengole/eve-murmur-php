dac208bab42f5e7e617f366dfa47c1df9925b37a
Ben Cole <wengole@gmail.com>
2010-11-26 15:05:16 +0000

    * controller/register.php: Copy/paste getCharacter code from v1 * Removed old config file * Removed old index.php * config/eveconfig.php: Created config for API etc.

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -8,6 +8,9 @@ class Register extends Controller {
         parent::Controller();
         $this->load->helper(array('html', 'form'));
         $this->load->library('Registration');
+        $this->load->library('Pheal/Pheal.php');
+        spl_autoload_register("Pheal::classload");
+        PhealConfig::getInstance()->cache = new PhealFileCache($pheal_cache);
         $this->reg = new Registration();
     }
 
@@ -35,8 +38,81 @@ class Register extends Controller {
     }
 
     function getCharacters() {
-        //TODO: Create uname_array with key of charID and value of charName
-        //
+        if (preg_match('/^[0-9]*\z/', $this->reg->userid)) {
+            $pheal = new Pheal($this->reg->userid, $this->reg->apikey);
+        } else {
+            $pheal = new Pheal('123456', 'abc123');
+        }
+        // On API errors switch to using cache files only
+        try {
+            $characters = $pheal->Characters();
+        } catch (Exception $exc) {
+            PhealConfig::getInstance()->cache = new PhealFileCacheForced($pheal_cache);
+            try {
+                $characters = $pheal->Characters();
+            } catch (Exception $exc) {
+                echo '<h3>You must provide a valid userID and API Key</h3>';
+                echo '<h3>User ID is a 6+ digit number<br />API Key is a 64 character string</h3>';
+                unset($_POST['userid']);
+                unset($_POST['apikey']);
+            }
+        }
+        $uname_array = array();
+        $query = "SELECT corpAllianceContactList.contactID FROM corpAllianceContactList
+                    JOIN corpCorporationSheet
+                    ON corpCorporationSheet.corporationID = corpAllianceContactList.ownerID
+                    WHERE corpCorporationSheet.allianceID = $allianceID
+                    AND standing > 0;";
+        $result = mysql_query($query, $conn);
+        $blues = array();
+        while ($row = mysql_fetch_array($result)) {
+            $blues[] = $row['contactID'];
+        }
+        if (isset($characters)) {
+            foreach ($characters->characters as $character) {
+                $pheal->scope = "char";
+                $charsheet = $pheal->CharacterSheet(array('characterID' => $character->characterID));
+                $pheal->scope = "corp";
+                $corpsheet = $pheal->CorporationSheet(array('corporationID' => $charsheet->corporationID));
+                switch ($corpOnly) {
+                    case true:
+                        if ($corpsheet->corporationID == $corpID || in_array($corpsheet->corporationID, $blues) || in_array($corpsheet->allianceID, $blues))
+                            $uname_array[] = $character->name;
+                        break;
+                    default:
+                        if ($corpsheet->allianceID == $allianceID || in_array($corpsheet->corporationID, $blues) || in_array($corpsheet->allianceID, $blues))
+                            $uname_array[] = $character->name;
+                        break;
+                }
+            }
+            if (!empty($uname_array)) {
+                echo "<p>Pick Character:</p>
+			<select id='userselect' name='username'>";
+                foreach ($uname_array as $username) {
+                    if (isset($_POST['username']) && $username == $_POST['username']) {
+                        echo "<option selected='selected'>$username";
+                    } else {
+                        echo "<option>$username";
+                    }
+                }
+                echo "</select>";
+            } else {
+                switch ($corpOnly) {
+                    case true:
+                        $pheal->scope = "eve";
+                        $corpName = $pheal->CharacterName(array("ids" => $corpID));
+                        $corpName = $corpName->characters[0]['name'];
+                        echo "<h3>No characters in or blue to $corpName on account!</h3>";
+                        break;
+                    default:
+                        $pheal->scope = "eve";
+                        $allyName = $pheal->CharacterName(array("ids" => $allianceID));
+                        $allyName = $allyName->characters[0]['name'];
+                        echo "<h3>No characters in or blue to $allyName on account!</h3>";
+                        break;
+                }
+            }
+        }
     }
 
 }
