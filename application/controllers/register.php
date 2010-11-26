a3a73356a0e914b37d081b4e743bba32d5921eb7
Ben Cole <wengole@gmail.com>
2010-11-26 11:30:39 +0000

    * register controller: Fixed $this->input

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -2,34 +2,41 @@
 
 class Register extends Controller {
 
+    private $reg;
+
     function Register() {
         parent::Controller();
         $this->load->helper(array('html', 'form'));
         $this->load->library('Registration');
-        $reg = new Registration();
+        $this->reg = new Registration();
     }
 
     function index() {
         $data['main_content'] = 'registerview';
         $data['title'] = 'Mumble Registration';
-        $data['data'] = $reg;
+        $data['data'] = $this->reg;
         $this->load->view('includes/template', $data);
     }
 
     function add() {
-        $reg->userid = $this->input('userid');
-        $reg->apikey = $this->input('apikey');
-        $reg->username = $this->input('username');
-        if(empty ($reg->uname_array) && !empty ($reg->userid) && !empty ($reg->apikey))
-                $reg->uname_array = $this->getCharacters();
-        if(!empty ($reg->username))
-                $reg->selected_user = $reg->getSelectedUser();
-        $reg->password = $this->input('password');
-        $reg->password2 = $this->input('password2');
+        $this->reg->userid = $this->input->post('userid');
+        $this->reg->apikey = $this->input->post('apikey');
+        $this->reg->username = $this->input->post('username');
+        if (empty($this->reg->uname_array) && !empty($this->reg->userid) && !empty($this->reg->apikey))
+            $this->reg->uname_array = $this->getCharacters();
+        if (!empty($this->reg->username))
+            $this->reg->selected_user = $this->reg->getSelectedUser();
+        $this->reg->password = $this->input->post('password');
+        $this->reg->password2 = $this->input->post('password2');
+        $data['main_content'] = 'registerview';
+        $data['title'] = 'Mumble Registration';
+        $data['data'] = $this->reg;
+        $this->load->view('includes/template', $data);
     }
 
     function getCharacters() {
         //TODO: Create uname_array with key of charID and value of charName
         //
     }
+
 }
