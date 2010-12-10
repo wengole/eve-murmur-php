d961da318427c67dcfbdfc79182fc9a0a2ba9c2b
Ben Cole <wengole@gmail.com>
2010-12-10 00:17:26 +0000

    * Started work on cron job

--- application/controllers/checkusers.php
+++ application/controllers/checkusers.php
@@ -1,15 +1,39 @@
 <?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+require_once 'Ice.php';
+require_once APPPATH . 'libraries/Murmur_1.2.2.php';
+
 class CheckUsers extends Controller {
-    
-    function CheckUsers() {
+
+    var $user; // This line to be removed, for NetBeans autocomplete only
+
+    function __construct() {
         parent::Controller();
+        $this->load->model('User');
+        $this->load->library('email');
     }
-    
+
     function index() {
-        // 1. Check users from MySQL and update their API details every hour
+        $this->user = new User(); // This line to be removed, for NetBeans autocomplete only
+        $this->user->setMurmurUsers();
+        $this->user->setDbUsers();
+        $dbUsers = $this->user->getDbUsers();
+        $murmurUsers = $this->user->getMurmurUsers();
+        $murmurUserIDs = array();
+        foreach ($murmurUsers as $id => $username) {
+            $murmurUserIDs[] = $id;
+        }
+        foreach ($dbusers as $userID => $user) {
+            if (!in_array($userID, $murmurUserIDs)) {
+                // TODO: Delete from DB userIDs not on mumble server
+                // Probably best to email results before *actually* doing this
+            }
+        }
     }
-    
+
 }
 
 ?>
