0e777b8c1c4a026038ce4de8ff80f1ae13e57536
Ben Cole <wengole@gmail.com>
2011-02-05 19:58:13 +0000

    * Edited Pheal __construct to be compatible with CI

--- application/libraries/pheal/Pheal.php
+++ application/libraries/pheal/Pheal.php
@@ -1,63 +1,59 @@
 <?php
+
 /*
- MIT License
- Copyright (c) 2010 Peter Petermann, Daniel Hoffend
+  MIT License
+  Copyright (c) 2010 Peter Petermann, Daniel Hoffend
 
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without
- restriction, including without limitation the rights to use,
- copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the
- Software is furnished to do so, subject to the following
- conditions:
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use,
+  copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following
+  conditions:
 
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
-*/
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /**
  * Pheal (PHp Eve Api Library), a EAAL Port for PHP
  */
-class Pheal
-{
+class Pheal {
+
     /**
      * Version container
      */
     public static $version = "0.0.13";
-
     /**
      * resource handler for curl
      * @static
      * @var resource
      */
     public static $curl;
-
     /**
      * @var int
      */
     private $userid;
-
     /**
      * @var string
      */
     private $key;
-
     /**
      * EVE Api scope to be used (for example: "account", "char","corp"...)
      * @var String 
      */
     public $scope;
-    
     /**
      * Result of the last XML request, so application can use the raw xml data
      * @var String 
@@ -70,11 +66,20 @@ class Pheal
      * @param string $key the EVE apikey
      * @param string $scope scope to use, defaults to account. scope can be changed during usage by modifycation of public attribute "scope"
      */
-    public function __construct($userid=null, $key=null, $scope="account")
-    {
-        $this->userid = $userid;
-        $this->key = $key;
-        $this->scope = $scope;
+    public function __construct($params = NULL) {
+        if (isset($params)) {
+            $this->userid = $params['userid'];
+            $this->key = $params['key'];
+            if (isset($params['scope'])) {
+                $this->userid = $params['userid'];
+            } else {
+                $this->scope = 'eve';
+            }
+        } else {
+            $this->userid = '123456';
+            $this->key = 'abc123';
+            $this->scope = 'eve';
+        }
     }
 
     /**
@@ -83,14 +88,12 @@ class Pheal
      * @param array $arguments an array of arguments
      * @return PhealResult
      */
-    public function  __call($name, $arguments)
-    {
-        if(count($arguments) < 1 || !is_array($arguments[0]))
+    public function __call($name, $arguments) {
+        if (count($arguments) < 1 || !is_array($arguments[0]))
             $arguments[0] = array();
         $scope = $this->scope;
         return $this->request_xml($scope, $name, $arguments[0]); // we only use the
         //first argument params need to be passed as an array, due to naming
-
     }
 
     /**
@@ -98,8 +101,7 @@ class Pheal
      * @param string $name name of the scope e.g. "mapScope"
      * @return mixed Pheal or null
      */
-    public function __get($name)
-    {
+    public function __get($name) {
         if (preg_match('/(.+)Scope$/', $name, $matches) == 1) {
             $this->scope = $matches[1];
             return $this;
@@ -113,44 +115,43 @@ class Pheal
      * @todo errorhandling
      * @return PhealResult
      */
-    private function request_xml($scope, $name, $opts)
-    {
+    private function request_xml($scope, $name, $opts) {
         $opts = array_merge(PhealConfig::getInstance()->additional_request_parameters, $opts);
-        if(!$this->xml = PhealConfig::getInstance()->cache->load($this->userid,$this->key,$scope,$name,$opts))
-        {
+        if (!$this->xml = PhealConfig::getInstance()->cache->load($this->userid, $this->key, $scope, $name, $opts)) {
             $url = PhealConfig::getInstance()->api_base . $scope . '/' . $name . ".xml.aspx";
-            if($this->userid) $opts['userid'] = $this->userid;
-            if($this->key) $opts['apikey'] = $this->key;
-            
+            if ($this->userid)
+                $opts['userid'] = $this->userid;
+            if ($this->key)
+                $opts['apikey'] = $this->key;
+
             try {
                 // start measure the response time
                 PhealConfig::getInstance()->log->start();
 
                 // request
-                if(PhealConfig::getInstance()->http_method == "curl" && function_exists('curl_init'))
-                    $this->xml = self::request_http_curl($url,$opts);
+                if (PhealConfig::getInstance()->http_method == "curl" && function_exists('curl_init'))
+                    $this->xml = self::request_http_curl($url, $opts);
                 else
-                    $this->xml = self::request_http_file($url,$opts);
+                    $this->xml = self::request_http_file($url, $opts);
 
                 // stop measure the response time
                 PhealConfig::getInstance()->log->stop();
 
                 // parse
                 $element = new SimpleXMLElement($this->xml);
-
-            } catch(Exception $e) {
+            } catch (Exception $e) {
                 // log + throw error
-                PhealConfig::getInstance()->log->errorLog($scope,$name,$opts,$e->getCode() . ': ' . $e->getMessage());
+                PhealConfig::getInstance()->log->errorLog($scope, $name, $opts, $e->getCode() . ': ' . $e->getMessage());
                 throw new PhealException('API Date could not be read / parsed, orginial exception: ' . $e->getMessage());
             }
-            PhealConfig::getInstance()->cache->save($this->userid,$this->key,$scope,$name,$opts,$this->xml);
-            
+            PhealConfig::getInstance()->cache->save($this->userid, $this->key, $scope, $name, $opts, $this->xml);
+
             // archive+save only non-error api calls + logging
-            if(!$element->error) {
-                PhealConfig::getInstance()->log->log($scope,$name,$opts);
-                PhealConfig::getInstance()->archive->save($this->userid,$this->key,$scope,$name,$opts,$this->xml);
+            if (!$element->error) {
+                PhealConfig::getInstance()->log->log($scope, $name, $opts);
+                PhealConfig::getInstance()->archive->save($this->userid, $this->key, $scope, $name, $opts, $this->xml);
             } else {
-                PhealConfig::getInstance()->log->errorLog($scope,$name,$opts,$element->error['code'] . ': ' . $element->error);
+                PhealConfig::getInstance()->log->errorLog($scope, $name, $opts, $element->error['code'] . ': ' . $element->error);
             }
         } else {
             $element = new SimpleXMLElement($this->xml);
@@ -166,56 +167,49 @@ class Pheal
      * @param array $opts an array of query paramters
      * @return string raw http response
      */
-    public static function request_http_curl($url,$opts)
-    {
+    public static function request_http_curl($url, $opts) {
         // init curl
-        if(!(is_resource(self::$curl) && get_resource_type(self::$curl) == 'curl'))
+        if (!(is_resource(self::$curl) && get_resource_type(self::$curl) == 'curl'))
             self::$curl = curl_init();
 
         // custom user agent
-        if(($http_user_agent = PhealConfig::getInstance()->http_user_agent) != false)
+        if (($http_user_agent = PhealConfig::getInstance()->http_user_agent) != false)
             curl_setopt(self::$curl, CURLOPT_USERAGENT, $http_user_agent);
-        
+
         // custom outgoing ip address
-        if(($http_interface_ip = PhealConfig::getInstance()->http_interface_ip) != false)
+        if (($http_interface_ip = PhealConfig::getInstance()->http_interface_ip) != false)
             curl_setopt(self::$curl, CURLOPT_INTERFACE, $http_interface_ip);
 
         // ignore ssl peer verification if needed
-        if(substr($url,5) == "https")
+        if (substr($url, 5) == "https")
             curl_setopt(self::$curl, CURLOPT_SSL_VERIFYPEER, PhealConfig::getInstance()->http_ssl_verifypeer);
-            
+
         // http timeout 
-        if(($http_timeout = PhealConfig::getInstance()->http_timeout) != false)
+        if (($http_timeout = PhealConfig::getInstance()->http_timeout) != false)
             curl_setopt(self::$curl, CURLOPT_TIMEOUT, $http_timeout);
-            
+
         // use post for params
-        if(count($opts) && PhealConfig::getInstance()->http_post)
-        {
+        if (count($opts) && PhealConfig::getInstance()->http_post) {
             curl_setopt(self::$curl, CURLOPT_POST, true);
             curl_setopt(self::$curl, CURLOPT_POSTFIELDS, $opts);
-        }
-        else
-        {
+        } else {
             curl_setopt(self::$curl, CURLOPT_POST, false);
-            
+
             // attach url parameters
-            if(count($opts))
-            $url .= "?" . http_build_query($opts);
+            if (count($opts))
+                $url .= "?" . http_build_query($opts);
         }
-        
+
         // additional headers
         $headers = array();
-        
+
         // enable/disable keepalive
-        if(($http_keepalive = PhealConfig::getInstance()->http_keepalive) != false)
-        {
+        if (($http_keepalive = PhealConfig::getInstance()->http_keepalive) != false) {
             curl_setopt(self::$curl, CURLOPT_FORBID_REUSE, false);
-            $http_keepalive = ($http_keepalive === true) ? 15 : (int)$http_keepalive;
+            $http_keepalive = ($http_keepalive === true) ? 15 : (int) $http_keepalive;
             $headers[] = "Connection: keep-alive";
             $headers[] = "Keep-Alive: timeout=" . $http_keepalive . ", max=1000";
-        }
-        else 
-        {
+        } else {
             curl_setopt(self::$curl, CURLOPT_FORBID_REUSE, true);
         }
 
@@ -226,21 +220,21 @@ class Pheal
         curl_setopt(self::$curl, CURLOPT_URL, $url);
         curl_setopt(self::$curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt(self::$curl, CURLOPT_RETURNTRANSFER, true);
-        
+
         // call
-        $result	= curl_exec(self::$curl);
+        $result = curl_exec(self::$curl);
         $errno = curl_errno(self::$curl);
         $error = curl_error(self::$curl);
 
-        if(!PhealConfig::getInstance()->http_keepalive)
+        if (!PhealConfig::getInstance()->http_keepalive)
             self::disconnect();
 
-        if($errno)
+        if ($errno)
             throw new Exception($error, $errno);
         else
             return $result;
     }
-    
+
     /**
      * method will do the actual http call using file()
      * remember: on some installations, file_get_contents(url) might not be available due to
@@ -249,31 +243,28 @@ class Pheal
      * @param array $opts an array of query paramters
      * @return string raw http response
      */
-    public static function request_http_file($url,$opts)
-    {
+    public static function request_http_file($url, $opts) {
         $options = array();
-        
+
         // set custom user agent
-        if(($http_user_agent = PhealConfig::getInstance()->http_user_agent) != false)
+        if (($http_user_agent = PhealConfig::getInstance()->http_user_agent) != false)
             $options['http']['user_agent'] = $http_user_agent;
-        
+
         // set custom http timeout
-        if(($http_timeout = PhealConfig::getInstance()->http_timeout) != false)
+        if (($http_timeout = PhealConfig::getInstance()->http_timeout) != false)
             $options['http']['timeout'] = $http_timeout;
-        
+
         // ignore ssl peer verification if needed
-        if(substr($url,5) == "https")
+        if (substr($url, 5) == "https")
             $options['ssl']['verify_peer'] = PhealConfig::getInstance()->http_ssl_verifypeer;
-        
+
         // use post for params
-        if(count($opts) && PhealConfig::getInstance()->http_post)
-        {
+        if (count($opts) && PhealConfig::getInstance()->http_post) {
             $options['http']['method'] = 'POST';
             $options['http']['content'] = http_build_query($opts);
         }
         // else build url parameters
-        elseif(count($opts))
-        {
+        elseif (count($opts)) {
             $url .= "?" . http_build_query($opts);
         }
 
@@ -283,27 +274,26 @@ class Pheal
 
         // create context with options and request api call
         // suppress the 'warning' message which we'll catch later with $php_errormsg
-        if(count($options)) 
-        {
+        if (count($options)) {
             $context = stream_context_create($options);
             $result = @file_get_contents($url, false, $context);
         } else {
             $result = @file_get_contents($url);
         }
 
-         // throw error
-        if($result === false) {
+        // throw error
+        if ($result === false) {
             $message = ($php_errormsg ? $php_errormsg : 'HTTP Request Failed');
-            
+
             // set track_errors back to the old value
-            ini_set('track_errors',$oldTrackErrors);
+            ini_set('track_errors', $oldTrackErrors);
 
             throw new Exception($message);
 
-        // return result
+            // return result
         } else {
             // set track_errors back to the old value
-            ini_set('track_errors',$oldTrackErrors);
+            ini_set('track_errors', $oldTrackErrors);
             return $result;
         }
     }
@@ -314,28 +304,26 @@ class Pheal
      * @static
      * @return void
      */
-    public static function disconnect()
-    {
-        if(is_resource(self::$curl) && get_resource_type(self::$curl) == 'curl')
+    public static function disconnect() {
+        if (is_resource(self::$curl) && get_resource_type(self::$curl) == 'curl')
             curl_close(self::$curl);
         self::$curl = null;
     }
-    
+
     /**
      * static method to use with spl_autoload_register
      * for usage include Pheal.php and then spl_autoload_register("Pheal::classload");
      * @param String $name
      * @return boolean
      */
-    public static function classload($name)
-    {
-        $dir = pathinfo(__FILE__, PATHINFO_DIRNAME) ."/";
-        if(substr($name, 0, 5) == "Pheal" && file_exists($dir . $name .".php"))
-        {
+    public static function classload($name) {
+        $dir = pathinfo(__FILE__, PATHINFO_DIRNAME) . "/";
+        if (substr($name, 0, 5) == "Pheal" && file_exists($dir . $name . ".php")) {
             require_once($dir . $name . ".php");
             return true;
         }
         return false;
     }
+
 }
 
