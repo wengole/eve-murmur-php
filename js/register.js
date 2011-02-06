77c97845cb98780bd09c751512e16fe95df8675b
Ben Cole <wengole@gmail.com>
2011-02-06 16:24:56 +0000

    * Moved js to separate file

--- js/register.js
+++ js/register.js
@@ -1,6 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function() {
+    $('#userIdInput').keyup(function(){
+        
+    });
+    $('#inputForm').submit(function(e) {
+        e.preventDefault();
+        $.post($(this).attr("action"), $(this).serialize(), function(data) {
+            $('#charDiv').show('fast');
+            $(data).each(function(i) {
+                $('#charSel').
+                append($('<option></option>').
+                    attr('value',$(this)[0].charid).
+                    text($(this)[0].name));
+            });
+        }, "json");
+    });
+});
